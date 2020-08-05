c894e6c3decc0f0377ac6a99456501450d00d026
馒头饭 <mhjlw@126.com>
2020-08-05 11:31:44 +0800

    :new:更新wga数据库

--- apidir/wga/data.php
+++ apidir/wga/data.php
@@ -4,6 +4,7 @@ $data = array(
     'shopxo.net' => '386392432',
     'yocoo.cn' => '392061164',
     'jooj.vip' => '7400686',
-    'zwvmall.com' => '1135139988'
+    'zwvmall.com' => '1135139988',
+    'hyhyni.com' => '2274473949'
 );
 ?>
