97e1c6ff150ad31c735f4fe9c4a4cb1ef1ed1e5e
馒头饭 <mhjlw@126.com>
2020-08-17 01:43:08 +0800

    :new:更新客户名单

--- apidir/wga/data.php
+++ apidir/wga/data.php
@@ -4,7 +4,8 @@ $data = array(
         'mantoufan.com' => '978572783',
         'hbycl.com' => '769665798',
         'beikar.com' => '769665798',
-        'l-ren.com.cn' => '414070709'
+        'l-ren.com.cn' => '414070709',
+        'ahuau.com' => '1791559110'
     ),
     'shopxoplugin_thirdpartylogin' => array(
         'mantoufan.com' => '978572783',
@@ -15,7 +16,8 @@ $data = array(
         'hyhyni.com' => '2274473949',
         'kdbg.cn' => '285982013',
         '103.81.169.241' => '735034910',
-        'haolidaojia.com' => '13207430743'
+        'haolidaojia.com' => '13207430743',
+        'b55b.cn' => '799992208'
     ),
     'shopxoplugin_servicepro' => array(
         'mantoufan.com' => '978572783'
