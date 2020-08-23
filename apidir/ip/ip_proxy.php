93d83428e54added17d7d9fbaf184232e1a18150
馒头饭 <mhjlw@126.com>
2020-08-23 19:25:02 +0800

    :new:增加代理IP库缓存

--- apidir/ip/ip_proxy.php
+++ apidir/ip/ip_proxy.php
@@ -7,9 +7,6 @@
     $mtfProxy = new mtfProxy();
     $mtfProxy->cacheTime = 360;
     $mtfProxy->dir['data'] = dirname(__FILE__) . '/cache';
-    if (!is_dir($mtfProxy->dir['data'])) {
-        mkdir($mtfProxy->dir['data']);
-    }
     $ips = $mtfProxy->get($g['ip_type']);
     if ($out_type === 'txt') {
         echo implode($g['out_sep'], $ips);
