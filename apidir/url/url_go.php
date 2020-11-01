04e732d6fd9a68eb5d17ae3bb5f225835161ac28
馒头饭 <mhjlw@126.com>
2020-11-01 17:58:48 +0800

    :new:增加DNS辅助节点加速功能

--- apidir/url/url_go.php
+++ apidir/url/url_go.php
@@ -4,6 +4,12 @@
     ), $g);
     $dm126 = $g['dm126'];
     include('data.php');
+    $wga = wga();
+    $data['dns'] = $wga['shopxoplugin_optimizer']['data']['domains'];
     include('url_go.html');
     exit;
+    function wga() {
+        include(dirname(__FILE__) . '/../wga/data.php');
+        return $data;
+    }
 ?>
