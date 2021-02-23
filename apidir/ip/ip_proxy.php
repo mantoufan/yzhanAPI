1c593af68e8e8bab5f909f55f78da480c6a814cb
吴小宇 <mhjlw@126.com>
2021-02-23 19:01:12 +0800

    :new:更新自动更新的代理IP库

--- apidir/ip/ip_proxy.php
+++ apidir/ip/ip_proxy.php
@@ -7,15 +7,16 @@
     $mtfProxy = new mtfProxy();
     $mtfProxy->cacheTime = 360;
     $mtfProxy->dir['data'] = dirname(__FILE__) . '/cache';
-    $ips = $mtfProxy->get($g['ip_type']);
     if ($out_type === 'txt') {
+        $ips = $mtfProxy->get($g['ip_type']);
         echo implode($g['out_sep'], $ips);
         exit;
     } else if ($out_type === 'html') {
         date_default_timezone_set('Asia/ShangHai');
+        $ips = array();$data = '';
         $cache = $mtfProxy->dir['data'] . '/ips_' . $g['ip_type'] . '.php';
+        include($cache);
         $html = file_get_contents(dirname(__FILE__) . '/tpl/ip_proxy.html');
-        $data = '';
         foreach($ips as $no => $ip) {
             list($ip, $port) = explode(':', $ip);
             $data .= '<tr' . ($no % 2 === 0 ? '' : ' class="pure-table-odd"') . '>';
@@ -24,7 +25,5 @@
         }
         echo str_replace('{{data}}', $data, $html);
         exit;
-    } else {
-
     }
 ?>
