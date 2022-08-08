bfd7e069c00fc36c39198f66549537ff9e94ce5a
馒头饭 <mhjlw@126.com>
2022-08-08 12:29:25 +0800

    :new:增加域名预解析和预连接功能，改善背景图片尺寸

--- apidir/url/url_go.php
+++ apidir/url/url_go.php
@@ -4,12 +4,12 @@
     ), $g);
     $dm126 = $g['dm126'];
     include('data.php');
-    $wga = wga();
-    $data['dns'] = $wga['shopxoplugin_optimizer']['data']['dns_domains'];
+    // $wga = wga();
+    // $data['dns'] = $wga['shopxoplugin_optimizer']['data']['dns_domains'];
     include('url_go.html');
     exit;
-    function wga() {
-        include(dirname(__FILE__) . '/../wga/data.php');
-        return $data;
-    }
+    // function wga() {
+    //     include(dirname(__FILE__) . '/../wga/data.php');
+    //     return $data;
+    // }
 ?>
