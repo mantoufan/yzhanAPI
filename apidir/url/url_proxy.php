8791c4238543457896025d4e90f67161f1cc08d0
root <root@k3s.y5.os120.com>
2022-09-05 17:40:18 +0800

    :rocket:增加 URL 反向 proxy 功能

---
+++ apidir/url/url_proxy.php
@@ -0,0 +1,11 @@
+<?php
+  $g = array_merge(array(
+    'url' => '' // url
+  ), $g);
+  $url = $g['url'];
+  if (!$url) output('缺少参数 网址：url', $out_type, -1, array('jsonp_cb' => $g['jsonp_cb']));
+  $whilteList = array('https://developers.google.com/static/search/apis/ipranges/googlebot.json');
+  if (in_array($url, $whilteList) === false) output('非白名单网址', $out_type, -1, array('jsonp_cb' => $g['jsonp_cb']));
+  echo file_get_contents($url);
+  exit;
+?>
