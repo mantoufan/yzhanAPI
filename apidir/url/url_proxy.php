62af0482d1c5b77e7c20432ae05b7d9784508598
root <root@k3s.y5.os120.com>
2022-09-07 01:50:57 +0800

    :art:url proxy 返回原始响应头

--- apidir/url/url_proxy.php
+++ apidir/url/url_proxy.php
@@ -6,6 +6,7 @@
   if (!$url) output('缺少参数 网址：url', $out_type, -1, array('jsonp_cb' => $g['jsonp_cb']));
   $whilteList = array('https://developers.google.com/static/search/apis/ipranges/googlebot.json');
   if (in_array($url, $whilteList) === false) output('非白名单网址', $out_type, -1, array('jsonp_cb' => $g['jsonp_cb']));
-  echo file_get_contents($url);
-  exit;
+  $c = file_get_contents($url);
+  foreach ($http_response_header as $header) header($header);
+  exit($c);
 ?>
